1d68dd26ce6226f20b03cedc34a6e3e4b52d3cc3
khannsameer <sk8717108@gmail.com>
2025-08-18 23:10:20 +0530

    modified in ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const BallCanvas = ({ icon }) => {
   return (
-    <div className="w-28 h-28 bg-gradient-to-br from-gray-800 to-gray-900 rounded-xl shadow-lg flex items-center justify-center p-4 hover:scale-105 transition-transform duration-300">
-      <img src={icon} alt="tech-icon" className="w-16 h-16 object-contain" />
-    </div>
+    <Tilt
+      glareEnable={true}
+      glareMaxOpacity={0.2}
+      scale={1.1}
+      transitionSpeed={2500}
+      tiltMaxAngleX={15}
+      tiltMaxAngleY={15}
+      className="w-28 h-28"
+    >
+      <div className="w-full h-full bg-gradient-to-br from-gray-800 to-gray-900 rounded-xl shadow-lg flex items-center justify-center p-4">
+        <img src={icon} alt="tech-icon" className="w-16 h-16 object-contain" />
+      </div>
+    </Tilt>
   );
 };
 
